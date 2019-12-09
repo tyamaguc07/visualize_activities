e9ef54031c0f8ebbc48676d8c2f1d5b10d7a257b
tyamaguc07 <tyamaguc07@gmail.com>
2019-12-09 22:45:39 +0900

    [modify] parse処理を細かくメソッド化

--- lib/visualize_activities/query/issues.rb
+++ lib/visualize_activities/query/issues.rb
@@ -7,29 +7,37 @@ module VisualizeActivities::Query
           since: target_time.iso8601,
       })
 
-      issues = result.data.repository.issues.nodes.map do |issue|
-        timeline_items = issue.timeline_items.edges.map(&:node).each_with_object([]) do |timeline_item, result|
-          result << case timeline_item.__typename
-                    when "CrossReferencedEvent"
-                      VisualizeActivities::TimelineItem::CrossReferencedEvent.new(timeline_item.actor.login, timeline_item.url, timeline_item.created_at)
-                    when "IssueComment"
-                      VisualizeActivities::TimelineItem::IssueComment.new(timeline_item.author.login, timeline_item.body_html, timeline_item.created_at)
-                    else
-                      next
-                    end
-        end
-
-        VisualizeActivities::Issue.new(
-                                      issue.title,
-                                      issue.body_html,
-                                      issue.assignees.nodes.map(&:login),
-                                      issue.url,
-                                      issue.created_at,
-                                      VisualizeActivities::TimelineItemSet.new(timeline_items),
-                                      )
-      end
+      issues = issues_mapper(result.data.repository.issues.nodes)
 
       VisualizeActivities::IssueSet.new(issues)
+    end
+
+    def self.issues_mapper(issues)
+      issues.map do |issue|
+        timeline_items = timeline_items_mapper(issue.timeline_items.edges.map(&:node))
+
+        VisualizeActivities::Issue.new(
+            issue.title,
+            issue.body_html,
+            issue.assignees.nodes.map(&:login),
+            issue.url,
+            issue.created_at,
+            VisualizeActivities::TimelineItemSet.new(timeline_items),
+            )
+      end
+    end
+
+    def self.timeline_items_mapper(timeline_items)
+      timeline_items.each_with_object([]) do |timeline_item, result|
+        result << case timeline_item.__typename
+                  when "CrossReferencedEvent"
+                    VisualizeActivities::TimelineItem::CrossReferencedEvent.new(timeline_item.actor.login, timeline_item.url, timeline_item.created_at)
+                  when "IssueComment"
+                    VisualizeActivities::TimelineItem::IssueComment.new(timeline_item.author.login, timeline_item.body_html, timeline_item.created_at)
+                  else
+                    next
+                  end
+      end
     end
 
     Query = VisualizeActivities::Client.parse <<-GraphQL
