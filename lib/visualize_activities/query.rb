b716a54f6b9877d87b8bdc041dec9a93d9e6d3f3
tyamaguc07 <tyamaguc07@gmail.com>
2019-12-12 10:58:30 +0900

    [add] PullRequest取得用のGraphQLクエリの実装

--- lib/visualize_activities/query.rb
+++ lib/visualize_activities/query.rb
@@ -1,1 +1,2 @@
 require 'visualize_activities/query/issues'
+require 'visualize_activities/query/pull_request'
---
+++ lib/visualize_activities/query/pull_request.rb
@@ -0,0 +1,48 @@
+module VisualizeActivities::Query
+  class PullRequest
+    def self.search(owner, repository, target, target_time)
+      return PullRequest.new(owner, repository, target, target_time).search
+    end
+
+    def initialize(owner, repository, target, target_time)
+      @owner = owner
+      @repository = repository
+      @target = target
+      @target_time = target_time
+    end
+
+    attr_reader :owner, :repository, :target, :target_time
+
+    def search
+    end
+
+    private
+
+    Query = VisualizeActivities::Client.parse <<-GraphQL
+query($owner: String!, $repository: String!, $since: DateTime!, $after: String) {
+  repository(owner: $owner, name: $repository) {
+    pullRequests(after: $after, orderBy: {field: CREATED_AT, direction: DESC}, first: 100) {
+      nodes {
+        bodyHTML
+        author {
+          login
+        }
+        reviews(first: 100) {
+          nodes {
+            author {
+              login
+            }
+            bodyHTML
+            createdAt
+          }
+        }
+        pageInfo {
+          endCursor
+        }
+      }
+    }
+  }
+}
+    GraphQL
+  end
+end
