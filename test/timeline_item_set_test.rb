b4ed85d26c4fbfe66f8d945c7513cea924accd05
tyamaguc07 <tyamaguc07@gmail.com>
2020-01-02 21:56:11 +0900

    [add] TimelineItemSetTest

---
+++ test/timeline_item_set_test.rb
@@ -0,0 +1,16 @@
+require 'minitest/autorun'
+
+class TimelineItemSetTest < MiniTest::Unit::TestCase
+  def setup
+  end
+
+  def test_comments
+    issue_comment_01 = VisualizeActivities::TimelineItem::IssueComment.new('test_username', 'test_content_01', Time.now)
+    issue_comment_02 = VisualizeActivities::TimelineItem::IssueComment.new('test_username', 'test_content_02', Time.now)
+    pull_request_01 = VisualizeActivities::TimelineItem::CrossReferencedEvent.new('test_username', 'https://github.com/#ref-pullrequest-', Time.now)
+
+    timeline_item_set = VisualizeActivities::TimelineItemSet.new([issue_comment_01, issue_comment_02, pull_request_01])
+
+    assert_equal([issue_comment_01, issue_comment_02], timeline_item_set.comments)
+  end
+end
